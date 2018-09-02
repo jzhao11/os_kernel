f31cc5b5067a94718724f95f12972d60ff9f1ebd
jzhao11 <35846410+jzhao11@users.noreply.github.com>
2018-09-01 21:34:02 -0700

    first commit for kernel src code

---
+++ kernel/shell.c
@@ -0,0 +1,8 @@
+#include <kernel.h>
+
+
+
+
+void start_shell()
+{
+}
---
+++ kernel/stdlib.c
@@ -0,0 +1,19 @@
+
+#include <kernel.h>
+
+
+int k_strlen(const char *str)
+{
+}
+
+void           *k_memcpy(void *dst, const void *src, int len)
+{
+}
+
+int k_memcmp(const void *b1, const void *b2, int len)
+{
+}
+
+void k_memset(const void *b, char fill, int len)
+{
+}
